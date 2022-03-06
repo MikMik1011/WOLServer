1c8075c030d32ee82e006527c8849320e12b0b0b
Misa Stefanovic <40996107+MikMikYT@users.noreply.github.com>
2022-03-06 23:24:58 +0100

    change variables to use .env file, add noip toggle and changeable noip cooldown, translate messages to english, add readme

---
+++ README.md
@@ -0,0 +1,35 @@
+# Intro
+
+
+## This is a REST server that can send WOL packets and also update your DDNS hostname. 
+
+### One of the use-cases is ie. if your router flushes your computer's MAC instantly, so you can't send the WOL packet to your computer regularly, but instead you need to use some kind of a server on the local network which can do that for you.
+
+
+# Usage
+
+
+### You need to make a `.env` file. There you need to put the following variables:
+
+
+```
+TARGET_MAC=
+SERVER_PORT=
+WOL_PORT=
+WEBHOOK_URL=
+USE_NOIP=True
+NOIP_CHECK_PERIOD=
+NOIP_USERNAME=
+NOIP_PASSWORD=
+NOIP_HOSTNAME=
+SERVER_LOGIN=
+SERVER_PASSWORD=
+```
+
+### The variable names are pretty much self-explanatory. 
+
+
+### Notice: If you don't want to use NOIP then change the USE_NOIP variable to anything other than "True" (it is case sensitive).
+
+
+## Enjoy the script i guess!
--- tabletServer.py
+++ tabletServer.py
@@ -1,9 +1,10 @@
 import os
 import requests
 import json
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 from wakeonlan import send_magic_packet
 from discord_webhook import DiscordWebhook
+from dotenv import load_dotenv, find_dotenv
 
 from threading import Event, Thread
 
@@ -15,10 +16,12 @@ def call_repeatedly(interval, func, *args):
     Thread(target=loop).start()    
     return stopped.set
 
-mac = "YOUR MAC HERE" 
-serverPort = 1234 #YOUR PORT HERE
-wolPort = 4343 #YOUR WOL PORT HERE
-webhookURL = "YOUR DISCORD WEBHOOK URL HERE"
+load_dotenv(find_dotenv())
+
+mac = os.getenv("TARGET_MAC").replace(":", ".")
+serverPort = int(os.getenv("SERVER_PORT")) 
+wolPort = int(os.getenv("WOL_PORT"))
+webhookURL = os.getenv("WEBHOOK_URL")
 
 oldIP = ""
 
@@ -32,31 +35,31 @@ def updateNOIP():
 
         if newIP != oldIP:
             oldIP = newIP
-            os.system("noipy -u your_nick_here -p your_pw_here -n your_ddns_url_here --provider noip")
-            DiscordWebhook(url=webhookURL, rate_limit_retry=True, content=f'DDNS IP azuriran, novi IP je: {newIP}').execute()
+            os.system("noipy -u {0} -p {1} -n {2} --provider noip".format(os.getenv("NOIP_USERNAME"), os.getenv("NOIP_PASSWORD"), os.getenv("NOIP_HOSTNAME")))
+            DiscordWebhook(url=webhookURL, rate_limit_retry=True, content=f'DDNS IP updated, new IP is: {newIP}').execute()
         else: 
             print("nije novi ip")
 
     except:
         print("offline")
 
-
-call_repeatedly(10, updateNOIP)
+if os.getenv("USE_NOIP") == "True":
+    call_repeatedly(int(os.getenv("NOIP_CHECK_PERIOD")), updateNOIP)
 
 app = Flask(__name__)
 
 
 @app.route('/wol', methods=['GET'])
 def sendWOL():
-    if request.args.get("sifra") == "YourPassHere":
+    if request.args.get("pass") == os.getenv("SERVER_PASSWORD"):
         send_magic_packet(mac, port = wolPort)
-        DiscordWebhook(url=webhookURL, rate_limit_retry=True, content=f'WOL paket poslat sa IPa: {request.environ["REMOTE_ADDR"]}').execute()
-        return "WOL paket poslat!" 
+        DiscordWebhook(url=webhookURL, rate_limit_retry=True, content=f'WOL packet sent from following IP: {request.environ["REMOTE_ADDR"]}').execute()
+        return "<h1>WOL packet sent successfully!<h1>" 
     else:
-        DiscordWebhook(url=webhookURL, rate_limit_retry=True, content=f'IP: {request.environ["REMOTE_ADDR"]} je pokusao da posalje WOL paket al se uvatio za kurac').execute()
-        return "ne znas sifru od fice"
+        DiscordWebhook(url=webhookURL, rate_limit_retry=True, content=f'IP: {request.environ["REMOTE_ADDR"]} accessed WOL page but didn\'t type the right password!').execute()
+        return "<h1>Invalid password!<h1>"
 
-app.run(host = '0.0.0.0', port = serverPort )
+app.run(host = '0.0.0.0', port = serverPort)
 
 
 
