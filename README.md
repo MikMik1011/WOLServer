cacefa06ed10fcd26c6cb1590a0e4ca5dfd7751a
Misa Stefanovic <40996107+MikMikYT@users.noreply.github.com>
2022-04-10 01:40:23 +0200

    add logs and other improvements
    - add new log system:
    -- make every log output with current time
    -- add log to file
    -- make log output to console, file and discord at the same time
    -- add new .env config: LOG_TO_FILE
    
    - add running check:
    -- make script log that it's running every X seconds (see next line)
    -- add new .env config: RUNNING_LOG_PERIOD
    
    - code refactor and reorder

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # Intro
 
-
-## This is a REST server that can send WOL packets and also update your DDNS hostname. 
+## This is a REST server that can send WOL packets and also update your DDNS hostname.
 
 ### One of the use-cases is ie. if your router flushes your computer's MAC instantly, so you can't send the WOL packet to your computer regularly, but instead you need to use some kind of a server on the local network which can do that for you.
 
@@ -9,18 +8,20 @@
 # Usage
 
 
-### Firstly, you need to install the required modules. List of them is in the `requirements.txt` file.
-### You also need to make a `.env` file. There you have to declare and initialize the following variables:
+## Firstly, you need to install the required modules. List of them is in the `requirements.txt` file.
+## You also need to make a `.env` file. There you have to declare and initialize the following variables:
 
 
 ```
 DEBUG=False
+LOG_TO_FILE=True
+RUNNING_LOG_PERIOD=3600
 TARGET_MAC=
 SERVER_PORT=
 WOL_PORT=
 WEBHOOK_URL=
 USE_NOIP=True
-NOIP_CHECK_PERIOD=
+NOIP_CHECK_PERIOD=150
 NOIP_USERNAME=
 NOIP_PASSWORD=
 NOIP_HOSTNAME=
@@ -28,10 +29,11 @@ SERVER_USERNAME=
 SERVER_PASSWORD=
 ```
 
-### The variable names are pretty much self-explanatory. 
+## The variable names are pretty much self-explanatory. 
 
+### Note: These preset values of period variables are recommended, but feel free to change them if you want to do stuff more or less frequently.
 
 ### Note: If you don't want to use NOIP then change the USE_NOIP variable to anything other than "True" (it is case sensitive). Also you can turn debug on if you want.
 
 
-## Enjoy the script I guess!
+### Enjoy the script I guess!
--- WOLServer.py
+++ WOLServer.py
@@ -3,23 +3,14 @@ startTime = time.time()
 
 import os
 import requests
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta 
 from flask import Flask, request, make_response, jsonify
 from wakeonlan import send_magic_packet
 from discord_webhook import DiscordWebhook
-from dotenv import load_dotenv, find_dotenv
 from threading import Event, Thread
 
-def debugPrint(args):
-    if os.getenv("DEBUG") == "True":
-        print(args)
-
-def srvResp(success):
-    return make_response(jsonify({
-            "date": datetime.now().strftime("%d.%m.%Y %H:%M:%S"),
-            "success": success, 
-            }), 200 if success else 401, 
-            {'WWW-Authenticate' : 'Basic realm="Enter credentials in order to send a WOL packet!"'} if not success else {})
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
 
 def call_repeatedly(interval, func, *args):
     stopped = Event()
@@ -29,14 +20,49 @@ def call_repeatedly(interval, func, *args):
     Thread(target=loop).start()    
     return stopped.set
 
-load_dotenv(find_dotenv())
-
 mac = os.getenv("TARGET_MAC").replace(":", ".")
 serverPort = int(os.getenv("SERVER_PORT")) 
 wolPort = int(os.getenv("WOL_PORT"))
 webhookURL = os.getenv("WEBHOOK_URL")
 
 oldIP = ""
+
+def log(args):
+    currTime = datetime.now().strftime("%H:%M:%S") 
+
+    print(f"[{currTime}] {args}") 
+
+    if os.getenv("LOG_TO_FILE") == "True":
+        try:
+            with open("wolserver.log", "a") as logFile: 
+                logFile.write(f"[{currTime}] {args}\n") 
+        except:
+            pass
+
+    try:
+        DiscordWebhook(url=webhookURL, rate_limit_retry=True, content=f"[{currTime}] {args}").execute() 
+    except:
+        pass
+
+def debugPrint(args):
+    if os.getenv("DEBUG") == "True": 
+        log(args)
+
+def resetLog():
+    if os.getenv("LOG_TO_FILE") == "True": 
+        try:
+            with open("wolserver.log", "w") as logFile:
+                logFile.write("")
+        except:
+            pass
+
+    try:
+        DiscordWebhook(url=webhookURL, rate_limit_retry=True, content="** **").execute() # ** ** sends a blank message to discord
+    except:
+        pass
+
+resetLog()
+
 
 def updateNOIP():
     try: 
@@ -50,17 +76,29 @@ def updateNOIP():
             oldIP = newIP
             os.system("noipy -u {0} -p {1} -n {2} --provider noip".format(os.getenv("NOIP_USERNAME"), os.getenv("NOIP_PASSWORD"), os.getenv("NOIP_HOSTNAME")))
             try:
-                DiscordWebhook(url=webhookURL, rate_limit_retry=True, content=f'DDNS IP updated, new IP is: {newIP}').execute()
+                log(f'DDNS IP updated, new IP is: {newIP}')
             except:
                 pass
         else: 
             debugPrint("IP hasn't changed!")
 
     except:
-        print("Failed to fetch your IP, maybe you are offline! (or maybe the ipify.org API is down)")
+        log("Failed to fetch your IP, maybe you are offline! (or maybe the ipify.org API is down)")
 
 if os.getenv("USE_NOIP") == "True":
-    call_repeatedly(int(os.getenv("NOIP_CHECK_PERIOD")), updateNOIP)
+    call_repeatedly(int(os.getenv("NOIP_CHECK_PERIOD")), updateNOIP) 
+    updateNOIP()
+
+call_repeatedly(int(os.getenv("RUNNING_LOG_PERIOD")), log, "Server is up and running!")
+
+
+def srvResp(success):
+    return make_response(jsonify({
+            "date": datetime.now().strftime("%d.%m.%Y %H:%M:%S"),
+            "success": success, 
+            }), 
+            200 if success else 401, 
+            {'WWW-Authenticate' : 'Basic realm="Enter credentials in order to send a WOL packet!"'} if not success else {}) 
 
 app = Flask(__name__)
 
@@ -75,18 +113,20 @@ def helloWorld():
 def sendWOL():
     if not request.authorization:
         return srvResp(False)
-    elif request.authorization.username == os.getenv("SERVER_USERNAME") and request.authorization.password == os.getenv("SERVER_PASSWORD"):
+    elif request.authorization.username == os.getenv("SERVER_USERNAME") and request.authorization.password == os.getenv("SERVER_PASSWORD"): 
         send_magic_packet(mac, port = wolPort)
         try:
-            DiscordWebhook(url=webhookURL, rate_limit_retry=True, content=f'{request.authorization.username} sent an WOL packet from the following IP: {request.environ["REMOTE_ADDR"]}').execute()
+            log(f'{request.authorization.username} sent an WOL packet from the following IP: {request.environ["REMOTE_ADDR"]}')
         except:
             pass
         return srvResp(True) 
     else:
         try:
-            DiscordWebhook(url=webhookURL, rate_limit_retry=True, content=f'IP: {request.environ["REMOTE_ADDR"]} didn\'t type the right password!').execute()
+            log(f'IP: {request.environ["REMOTE_ADDR"]} didn\'t type the right password!')
         except:
             pass
         return srvResp(False)
 
+
+log("Flask server is starting!")
 app.run(host = '0.0.0.0', port = serverPort)
